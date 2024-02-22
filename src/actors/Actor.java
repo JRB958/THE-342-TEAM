817baddb8a664404a604b6816c20d49f2e1efad2
Houssam Righi <hrighi01@gmail.com>
2024-02-22 13:25:14 -0500

    Refactor actor classes and add new constructors

--- src/actors/Actor.java
+++ src/actors/Actor.java
@@ -1,8 +1,51 @@
 package actors;
+
 public abstract class Actor {
+
+    public Auth auth;
     private Boolean registered;
     private Boolean read;
     private Boolean write;
 
-    //a view method?
+    
+
+    public Actor(Auth auth, Boolean registered, Boolean read, Boolean write) {
+        this.auth = auth;
+        this.registered = registered;
+        this.read = read;
+        this.write = write;
+    }
+
+    public Auth getAuth() {
+        return auth;
+    }
+
+    public void setAuth(Auth auth) {
+        this.auth = auth;
+    }
+
+    public Boolean getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Boolean registered) {
+        this.registered = registered;
+    }
+
+    public Boolean getRead() {
+        return read;
+    }
+
+    public void setRead(Boolean read) {
+        this.read = read;
+    }
+
+    public Boolean getWrite() {
+        return write;
+    }
+
+    public void setWrite(Boolean write) {
+        this.write = write;
+    }
+
 }
